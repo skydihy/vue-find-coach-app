de2627250aab7cfbdff3d609f85b84167fd91bd5
skydihy <guylaxy.dev@gmail.com>
2022-11-25 15:54:24 +0700

    Update: get coaches from db

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,8 @@
+import { BASEURL } from '../../../constants';
+
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       id: context.rootGetters.userId,
       firstName: data.firstName,
@@ -9,6 +12,44 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const res = await fetch(BASEURL + `coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    });
+
+    await res.json();
+
+    if (!res.ok) {
+      // Error handling
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
+  },
+
+  async loadCoaches(context) {
+    const res = await fetch(BASEURL + `coaches.json`);
+
+    const resData = await res.json();
+
+    if (!res.ok) {
+      throw new Error(resData.message || 'Failed to fetch!');
+    }
+
+    const coaches = [];
+
+    for (const key in resData) {
+      const coach = {
+        id: key,
+        firstName: resData[key].firstName,
+        lastName: resData[key].lastName,
+        description: resData[key].description,
+        hourlyRate: resData[key].hourlyRate,
+        areas: resData[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };
