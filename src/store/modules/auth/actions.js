6739deba2e568fd4d8232df774f4c573bfadf8c0
skydihy <guylaxy.dev@gmail.com>
2022-11-26 15:20:05 +0700

    Update: auto login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,7 +2,27 @@ import { APIKEY, AUTH_URL } from '../../../constants';
 
 export default {
   async signin(context, payload) {
-    const url = `${AUTH_URL}:signInWithPassword?key=${APIKEY}`;
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+
+  async signup(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = `${AUTH_URL}:signInWithPassword?key=${APIKEY}`;
+
+    if (mode === 'signup') {
+      url = `${AUTH_URL}:signUp?key=${APIKEY}`;
+    }
+
     const res = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
@@ -17,6 +37,9 @@ export default {
     if (!res.ok) {
       throw new Error(resData.message || 'Failed to authenticate');
     }
+
+    localStorage.setItem('token', resData.idToken);
+    localStorage.setItem('userId', resData.localId);
 
     context.commit('setUser', {
       token: resData.idToken,
@@ -25,28 +48,17 @@ export default {
     });
   },
 
-  async signup(context, payload) {
-    const url = `${AUTH_URL}:signUp?key=${APIKEY}`;
-    const res = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-
-    const resData = await res.json();
-
-    if (!res.ok) {
-      throw new Error(resData.message || 'Failed to authenticate');
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
     }
-
-    context.commit('setUser', {
-      token: resData.idToken,
-      userId: resData.localId,
-      tokenExpiration: resData.expiresIn,
-    });
   },
 
   logout(context) {
